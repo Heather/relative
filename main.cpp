82200f674adf756368365fdefd13db2f7dd63b6c
Heather <heather@live.ru>
2015-04-07 16:58:22 +0400

    cosmetics

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
-#include <windows.h>
 #include <iostream>
 
 #include "Shlwapi.h"
 
 int main(int argc, char *argv[]) {
-  char szOut[MAX_PATH] = "";
   if (argc < 2) {
-    std::cout << "Usage:" << std::endl;
-    std::cout << "  relative <from> <to>" << std::endl;
-    return 0;
+    std::cout << "Usage:" << std::endl
+              << "  relative <from> <to>" << std::endl;
+  } else {
+    char szOut[MAX_PATH] = "";
+    PathRelativePathTo(szOut,
+                       argv[1],
+                       FILE_ATTRIBUTE_DIRECTORY,
+                       argv[2],
+                       FILE_ATTRIBUTE_NORMAL);
+    std::cout << szOut;
   }
-  PathRelativePathTo(szOut,
-                     argv[1],
-                     FILE_ATTRIBUTE_DIRECTORY,
-                     argv[2],
-                     FILE_ATTRIBUTE_NORMAL);
-  std::cout << szOut;
   return 0;
 }
