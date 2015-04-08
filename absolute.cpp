dcad8460970f88ca7fc6bcd5e5ee615cec99e0be
Heather <heather@live.ru>
2015-04-08 11:58:18 +0400

    add absolute util to get absolute path based on two relative

---
+++ absolute.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+#include <tchar.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage:" << std::endl
+              << "  absolute <from> <to>" << std::endl;
+  } else {
+    if(!SetCurrentDirectory(argv[1])) {
+      printf("SetCurrentDirectory failed (%d)\n", GetLastError());
+      return -1;
+    }
+    TCHAR buffer[MAX_PATH]=TEXT(""); 
+    TCHAR** lppPart={NULL};
+    auto retval = GetFullPathName(argv[2],
+                  MAX_PATH,
+                  buffer,
+                  lppPart);
+    std::cout << buffer;
+  }
+  return 0;
+}
